74eaf5be62c0944a8b577d417ac72621f064e503
Dripcoding <dripcoding@gmail.com>
2026-01-19 15:58:55 -0700

    allow player sprite to wrap around

--- player.gd
+++ player.gd
@@ -16,3 +16,20 @@ func _physics_process(_delta: float) -> void:
 		global_rotation_degrees -= ship_rotation
 	elif Input.is_action_pressed("right"):
 		global_rotation_degrees += ship_rotation
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	var viewport_rect: Rect2 = get_viewport_rect()
+#	# wrap around x axis from left to right
+	if global_position.x < viewport_rect.position.x:
+		global_position.x = viewport_rect.end.x
+	# wrap around x axis from right to left
+	elif global_position.x > viewport_rect.end.x:
+		global_position.x = viewport_rect.position.x
+	
+	# wrap around y axis from top to bottom
+	if global_position.y < viewport_rect.position.y:
+		global_position.y = viewport_rect.end.y
+	# wrap around y axis from bottom to top
+	elif global_position.y > viewport_rect.end.y:
+		global_position.y = viewport_rect.position.y
