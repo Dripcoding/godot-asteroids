d12481ae0c27709deb0bbff677f1b895f1bba742
Dripcoding <dripcoding@gmail.com>
2026-01-24 16:55:41 -0700

    free lasers

--- asteroid.gd
+++ asteroid.gd
@@ -11,6 +11,6 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group('lasers'):
+		area.destroy()
 		health -= 1
 		print('health ' + str(health))
-		#area.queue_free()
--- laser.gd
+++ laser.gd
@@ -5,8 +5,17 @@ extends Area2D
 
 
 func _ready() -> void:
-	add_to_group('lasers')	
+	add_to_group('lasers')
+	%VisibleOnScreenNotifier2D.screen_exited.connect(_on_screen_exited)
 
 
 func _physics_process(delta: float) -> void:
 	position += Vector2.UP.rotated(global_rotation) * speed * delta
+
+
+func destroy() -> void:
+	queue_free()
+
+
+func _on_screen_exited() -> void:
+	destroy()
