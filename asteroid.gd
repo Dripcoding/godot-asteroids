20d35e16a508209ab10c74ff8e8b2499446bf48e
Dripcoding <dripcoding@gmail.com>
2026-01-24 16:52:13 -0700

    detect collisions between laser and asteroid

--- asteroid.gd
+++ asteroid.gd
@@ -7,3 +7,10 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method('take_damage'):
 		body.take_damage()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group('lasers'):
+		health -= 1
+		print('health ' + str(health))
+		#area.queue_free()
--- laser.gd
+++ laser.gd
@@ -4,5 +4,9 @@ extends Area2D
 @export var speed: float = 300.0
 
 
+func _ready() -> void:
+	add_to_group('lasers')	
+
+
 func _physics_process(delta: float) -> void:
 	position += Vector2.UP.rotated(global_rotation) * speed * delta
