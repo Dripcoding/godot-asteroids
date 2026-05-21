7bec3d92dc6bf065a8ac97e15da6f87f5fd1be6a
Dripcoding <dripcoding@gmail.com>
2026-05-20 23:20:36 -0600

    impl power up despawn

--- power_up_spawner.gd
+++ power_up_spawner.gd
@@ -1,7 +1,7 @@
 extends Timer
 
 
-@export var current_powerup: String = ''
+@export var current_powerup: Node2D = null
 @export var powerup_pos_offset: float = 50.0
 
 
@@ -18,7 +18,11 @@ var viewport_size: Vector2i = DisplayServer.window_get_size()
 
 func _on_timeout() -> void:
 	_spawn_powerup()
-	
+
+
+func _on_power_up_despawn_timer_timeout() -> void:
+	current_powerup.queue_free()
+	%PowerUpDespawnTimer.stop()
 	
 func _determine_powerup_position() -> Vector2:
 	var position_x: float = rng.randf_range(powerup_pos_offset, viewport_size.x - powerup_pos_offset)
@@ -30,5 +34,6 @@ func _spawn_powerup() -> void:
 	print('SPAWNING POWERUP')
 	var powerup = powerups['piercing_laser'].instantiate()
 	powerup.global_position = _determine_powerup_position()
+	current_powerup = powerup
 	add_child(powerup)
-	
+	%PowerUpDespawnTimer.start()
